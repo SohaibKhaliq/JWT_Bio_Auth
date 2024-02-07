92fc131dbacdd1724fb662ff9ecdef7c17584bcf
Sohaib Khaliq <sohaibkhaliq510@gmail.com>
2024-02-07 17:54:39 +0000

    Initialized the Empty React Project for client side!!

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h4>Welcome To Client side!!</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
