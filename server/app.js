42e2965c62c33438945968687f8a2788a4465210
Sohaib Khaliq <sohaibkhaliq510@gmail.com>
2024-02-07 18:07:33 +0000

    created simple express server and revelead simple environment variables

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
-const app= require('express');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
-app.listen(3000, () => {
-  console.log(`Backend Server is running on port 3000`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
